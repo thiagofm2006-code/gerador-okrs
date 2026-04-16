de5bf2d0724ab4a0c386058951f24c8bce508c08
thiagofm2006-code <thiagofm2006@gmail.com>
2026-04-15 22:45:54 -0300

    Delete Footer.tsx

--- Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center pt-4 pb-8">
-      <p className="text-xs text-gray-400">Gerado com IA para Product Owners</p>
-    </footer>
-  );
-}
